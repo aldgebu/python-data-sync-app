5bbc2ca65843fff71baa44ef38998f796638e9c2
aldgebu <aldgebu@gmail.com>
2025-07-08 20:33:42 +0400

    jwt setup creation

---
+++ dals/jwt_dal.py
@@ -0,0 +1,33 @@
+import datetime
+
+from typing import Optional
+from sqlalchemy import and_
+
+from models.jwt.jwt_type_enum import JWTTypeEnum
+from models.jwt.jwt_blocklist import JWTBlocklist
+from models.general.db_session_manager import DBSessionManager
+
+from dals.base_dal import BaseDAL
+
+
+class TokenDAL(BaseDAL):
+    def __init__(self):
+        self.JWTBlocklistModel = JWTBlocklist
+        self.session = DBSessionManager.get_session()
+
+    def create_blocklisted_token(self, jti: str, token_type: JWTTypeEnum, save_to_db: Optional[bool] = True):
+        token = self.JWTBlocklistModel(jti=jti, token_type=token_type)
+
+        if save_to_db:
+            self.save_to_db(token)
+        return token
+
+    def is_blocklisted(self, jti) -> bool:
+        token = self.session.query(self.JWTBlocklistModel).filter_by(jti=jti).first()
+        return token is not None
+
+    def clean_blocklist(self):
+        utcnow = datetime.datetime.now(datetime.UTC)
+        self.session.query(self.JWTBlocklistModel).filter(and_(self.JWTBlocklistModel.token_type == 'access',
+                                                               self.JWTBlocklistModel.valid_until < utcnow)
+                                                          ).delete()
---
+++ jwt_setup.py
@@ -0,0 +1,40 @@
+from flask_jwt_extended import JWTManager
+
+from dals.jwt_dal import TokenDAL
+
+jwt = JWTManager()
+
+
+@jwt.token_in_blocklist_loader
+def check_if_token_revoked(_jwt_header, jwt_payload):
+    jti = jwt_payload['jti']
+    token_dal = TokenDAL()
+
+    is_blocklisted = token_dal.is_blocklisted(jti)
+    if is_blocklisted:
+        print(f'Tried to use blocklisted {jti=}') # maybe to log somewhere
+
+    return is_blocklisted
+
+
+@jwt.user_identity_loader
+def user_identity_loader(user):
+    return user.id
+
+
+@jwt.user_lookup_loader
+def user_lookup_callback(_jwt_header, jwt_data):
+    user_id = jwt_data['sub']
+    # TODO: return actual user
+
+
+@jwt.unauthorized_loader
+def unauthorized_callback(reason):
+    print("Missing Authorization Header") # should be logged actually
+    return {"msg": reason}, 401
+
+
+@jwt.invalid_token_loader
+def invalid_token_callback(reason):
+    print("Invalid Authorization Header") # again and again
+    return {"msg": reason}, 401
