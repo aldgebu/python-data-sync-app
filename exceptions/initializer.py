b326cd2c36d79343aed24df9034d942182f305a0
aldgebu <aldgebu@gmail.com>
2025-07-08 22:04:57 +0400

    initialize user exceptions

--- exceptions/initializer.py
+++ exceptions/initializer.py
@@ -1,7 +1,9 @@
 from flask import Flask
 
+from exceptions.user_exceptions import user_exceptions
 from exceptions.general_exceptions import general_exceptions
 
 
 def initialize_exceptions(app: Flask):
     general_exceptions(app=app)
+    user_exceptions(app=app)
