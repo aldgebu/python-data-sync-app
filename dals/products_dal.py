47e0277bdbf87a5c9ee5b31be31b76be45d21c42
aldgebu <aldgebu@gmail.com>
2025-07-09 17:36:18 +0400

    adding transient = True

--- dals/products_dal.py
+++ dals/products_dal.py
@@ -1,5 +1,17 @@
+from typing import List
+
+from models.products import Products
+from models.general.db_session_manager import DBSessionManager
+
 from dals.base_dal import BaseDAL
 
 
 class ProductDAL(BaseDAL):
-    pass
+    def __init__(self):
+        self.db_session = DBSessionManager.get_session()
+
+    def update(self, products: List[Products]):
+        for product in products:
+            self.save_to_db(product, merge=True)
+
+        self.db_session.commit()
--- schemas/products_schema.py
+++ schemas/products_schema.py
@@ -2,7 +2,8 @@ from models.products import Products
 from schemas.ma import ma
 
 
-class ProductsSchema(ma.SQLAlchemySchema):
+class ProductsSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Products
+        transient = True
         load_instance = True
