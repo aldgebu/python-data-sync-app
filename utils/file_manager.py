922e100062781be2357d02d901d60ed35bee3f07
aldgebu <aldgebu@gmail.com>
2025-07-09 17:35:57 +0400

    adding blank line for readability

--- utils/file_manager.py
+++ utils/file_manager.py
@@ -7,6 +7,7 @@ class FileManager:
     def get_rows(cls, file: FileStorage):
         file_name = file.filename
         file.stream.seek(0)
+
         if file_name.endswith(".csv"):
             df = pandas.read_csv(file.stream)
         elif file_name.endswith(".xlsx"):
