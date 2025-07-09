b8bf0a01042675a6d9b4956245700c68fa8fddfa
aldgebu <aldgebu@gmail.com>
2025-07-09 23:06:14 +0400

    adding after_request procedure

--- app_setup.py
+++ app_setup.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 from models.general.db import setup_db
+from models.general.db_session_manager import DBSessionManager
+
 from exceptions.initializer import initialize_exceptions
 
 
@@ -9,3 +11,12 @@ def app_setup(app: Flask):
         setup_db(app)
 
     initialize_exceptions(app)
+
+    @app.after_request
+    def after_request(response):
+        success =  DBSessionManager.commit_session()
+
+        if not success:
+            response = jsonify({"message": "Unknown Problem"})
+            response.status_code = 500
+        return response
