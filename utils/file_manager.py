640ac07c3872dabcf9b60f923663784b465b07dd
aldgebu <aldgebu@gmail.com>
2025-07-09 16:59:17 +0400

    creating filemanager for reading csv and xlsx files

---
+++ utils/file_manager.py
@@ -0,0 +1,18 @@
+import pandas
+from werkzeug.datastructures import FileStorage
+
+
+class FileManager:
+    @classmethod
+    def get_rows(cls, file: FileStorage):
+        file_name = file.filename
+        file.stream.seek(0)
+        if file_name.endswith(".csv"):
+            df = pandas.read_csv(file.stream)
+        elif file_name.endswith(".xlsx"):
+            df = pandas.read_excel(file.stream)
+        else:
+            raise ValueError("Unsupported file format")
+
+        for _, row in df.iterrows():
+            yield row.to_dict()
