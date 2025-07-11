9a11654fa11bcd07f879e4d23f410ddddf96fd9d
aldgebu <aldgebu@gmail.com>
2025-07-11 12:54:59 +0400

    logging errors during db save and remove

--- dals/base_dal.py
+++ dals/base_dal.py
@@ -4,6 +4,10 @@ from abc import ABC
 
 from models.general.db_session_manager import DBSessionManager
 
+from exceptions.general_exceptions import UnKnownProblemException
+
+from utils.logs.log_manager import LogManager
+
 
 class BaseDAL(ABC):
     def __init__(self):
@@ -31,9 +35,20 @@ class BaseDAL(ABC):
                 self.db_session.flush()
             if commit:
                 self.db_session.commit()
-        except Exception as e:
-            print(e) #  actually we should log it somewhere
-            self.db_session.rollback()
+        except Exception:
+            LogManager.get_logger().critical(
+            f'Error during {BaseDAL.__name__}.{BaseDAL.save_to_db.__name__} : {entity}, flush={flush}, commit={commit}',
+                exc_info=True
+            )
+            try:
+                self.db_session.rollback()
+            except Exception:
+                LogManager.get_logger().critical(
+                    f'Error during {BaseDAL.__name__}.{BaseDAL.save_to_db.__name__} -> rollback : '
+                    f'{entity}, flush={flush}, commit={commit}',
+                    exc_info=True
+                )
+            raise UnKnownProblemException()
 
     def remove_from_db(self, entity, flush: Optional[bool] = False, commit: Optional[bool] = False):
         try:
@@ -42,6 +57,18 @@ class BaseDAL(ABC):
                 self.db_session.flush()
             if commit:
                 self.db_session.commit()
-        except Exception as e:
-            print(e) #  same here, we should log it somewhere
-            self.db_session.rollback()
+        except Exception:
+            LogManager.get_logger().critical(
+                f'Error during {BaseDAL.__name__}.{BaseDAL.remove_from_db.__name__} : '
+                f'{entity}, flush={flush}, commit={commit}',
+                exc_info=True
+            )
+            try:
+                self.db_session.rollback()
+            except Exception:
+                LogManager.get_logger().critical(
+                    f'Error during {BaseDAL.__name__}.{BaseDAL.remove_from_db.__name__} -> rollback : '
+                    f'{entity}, flush={flush}, commit={commit}',
+                    exc_info=True
+                )
+            raise UnKnownProblemException()
